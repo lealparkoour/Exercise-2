1184051a4ef84ae7cfffa6392ef6ce4dd917671c
sebastian leal <ghostsebas@hotmail.com>
2021-03-24 00:11:51 -0500

    1

---
+++ Sumofdigits/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Sumofdigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n ;
+            Console.Write("enter the number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Sum(n));
+          
+
+           
+
+            static int  Sum (int n)
+            {
+                if (n == 0)
+                    return (0);
+                else
+                    return ((n % 10) + Sum(n / 10));
+
+            }
+
+        }
+    }
+}
